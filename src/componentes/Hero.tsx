a9713d3802d5730950cc986c5a2940ad8387cf56
frqn2003 <crizangel87@gmail.com>
2025-10-02 16:04:48 -0300

    Mejoras a navbar y realizacion de Hero section + instalacion de lucide react

---
+++ src/componentes/Hero.tsx
@@ -0,0 +1,19 @@
+import Boton from './Boton'
+
+export default function Hero() {
+    return (
+        <section className="relative w-full h-screen overflow-hidden snap-y snap-mandatory">
+            <div className="bg-[url('/luxury-hotel-lobby-modern.png')] bg-cover bg-center absolute inset-0 z-10"></div>
+            <div className="bg-white/50 w-full h-full absolute z-20"></div>
+            <div className="contenedor relative z-30 flex h-full">
+                <div className="flex items-center justify-start w-full">
+                    <div className="flex flex-col items-start space-y-3 md:space-y-4 w-full">
+                        <h1 className="text-3xl md:text-5xl lg:text-6xl font-bold text-black leading-tight">Frase para atrapar la atención</h1>
+                        <p className="text-lg md:text-xl lg:text-2xl text-gray-900/90">Descripción corta del hotel/página</p>
+                        <Boton texto="Habitaciones" bgColor='bg-black hover:bg-black/90'/>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
