6aa0ec05b7efc73922580db15b29fa4e7a35a828
frqn2003 <crizangel87@gmail.com>
2025-10-26 12:51:49 -0300

    a

--- src/app/api/habitaciones/[id]/route.ts
+++ src/app/api/habitaciones/[id]/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
-/* hello */
+
 // ACTUALIZAR UNA HABITACIÓN
 export async function PUT(
     request: NextRequest,
