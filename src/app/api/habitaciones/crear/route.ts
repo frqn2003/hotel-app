d5350539e5e831c3956a7d28ecdf200967b4a13c
frqn2003 <crizangel87@gmail.com>
2025-10-26 13:01:09 -0300

    volviendo atra

--- src/app/api/habitaciones/crear/route.ts
+++ src/app/api/habitaciones/crear/route.ts
@@ -1,4 +1,4 @@
-/* import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 
 // CREAR UNA NUEVA HABITACIÓN (para panel de administrador)
@@ -57,4 +57,3 @@ export async function POST(request: NextRequest) {
         );
     }
 }
- */
