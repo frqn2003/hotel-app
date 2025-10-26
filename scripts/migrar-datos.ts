f78bcb820b6b4d89adba383db8107e17e5acda3f
frqn2003 <crizangel87@gmail.com>
2025-10-26 13:11:39 -0300

    queonda

--- scripts/migrar-datos.ts
+++ scripts/migrar-datos.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client'
+/* import { PrismaClient } from '@prisma/client'
 import * as fs from 'fs'
 import * as path from 'path'
 
@@ -124,4 +124,4 @@ async function main() {
     }
 }
 
-main()
+main() */
--- scripts/seed-rooms.ts
+++ scripts/seed-rooms.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client'
+/* import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
@@ -98,3 +98,4 @@ async function main() {
 }
 
 main()
+ */
